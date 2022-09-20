5331a3890d1e9bc609a9b0697a37d077ea77ee74
Rafael Muzel <rafaelmuzel@yahoo.com.br>
2022-09-20 19:02:37 +0100

    Criando a classe posição

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Chess
 {
@@ -10,6 +7,9 @@ namespace Chess
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("Posição: " + p);
+            Console.ReadLine();
         }
     }
 }
